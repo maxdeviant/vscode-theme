e4a557ef7a6cae5b411fcb404e7c98be30bb2ff3
Marshall Bowers <elliott.codes@gmail.com>
2024-01-28 21:32:31 -0500

    Remove unused `colorRegistry.ts` parsing code

--- crates/codegen/src/main.rs
+++ crates/codegen/src/main.rs
@@ -4,10 +4,6 @@ use std::io::Write;
 use anyhow::Result;
 use heck::{ToSnakeCase, ToUpperCamelCase};
 use indexmap::IndexMap;
-use oxc::allocator::Allocator;
-use oxc::ast::ast;
-use oxc::parser::Parser;
-use oxc::span::SourceType;
 use proc_macro2::{Ident, TokenStream};
 use quote::{format_ident, quote};
 
@@ -18,9 +14,6 @@ struct ThemeColor {
 }
 
 fn main() -> Result<()> {
-    // let color_registry_src = include_str!("../../../colorRegistry.ts");
-    // let colors = parse_registry_colors(&color_registry_src)?;
-
     let theme_color_src = include_str!("../theme-color.md");
     let colors = parse_theme_colors(&theme_color_src)?;
     let colors = group_colors(colors);
@@ -109,107 +102,6 @@ fn parse_theme_colors(src: &str) -> Result<IndexMap<String, ThemeColor>> {
     }
 
     Ok(colors)
-}
-
-fn parse_registry_colors(ts_src: &str) -> Result<IndexMap<String, ThemeColor>> {
-    let mut colors = IndexMap::new();
-
-    let allocator = Allocator::default();
-
-    let parser = Parser::new(
-        &allocator,
-        ts_src,
-        SourceType::default().with_typescript(true),
-    )
-    .parse();
-
-    if !parser.errors.is_empty() {
-        dbg!(&parser.errors);
-    }
-
-    for color in parser.program.body.into_iter().filter_map(parse_color_decl) {
-        colors.insert(color.name.clone(), color);
-    }
-
-    Ok(colors)
-}
-
-fn parse_color_decl(stmt: ast::Statement) -> Option<ThemeColor> {
-    let ast::Statement::ModuleDeclaration(module_decl) = stmt else {
-        return None;
-    };
-
-    let ast::ModuleDeclaration::ExportNamedDeclaration(named_decl) = module_decl.unbox() else {
-        return None;
-    };
-
-    let ast::Declaration::VariableDeclaration(variable_decl) = named_decl.unbox().declaration?
-    else {
-        return None;
-    };
-
-    if !variable_decl.kind.is_const() || variable_decl.declarations.is_empty() {
-        return None;
-    }
-
-    let variable_decl = &variable_decl.declarations[0];
-
-    let ast::Expression::CallExpression(call_expr) = variable_decl.init.as_ref()? else {
-        return None;
-    };
-
-    let ast::Expression::Identifier(callee) = &call_expr.callee else {
-        return None;
-    };
-
-    if callee.name != "registerColor" {
-        return None;
-    };
-
-    let ast::Argument::Expression(ast::Expression::StringLiteral(color_name_lit)) =
-        call_expr.arguments.get(0)?
-    else {
-        return None;
-    };
-
-    let ast::Argument::Expression(ast::Expression::CallExpression(call_expr)) =
-        call_expr.arguments.get(2)?
-    else {
-        return None;
-    };
-
-    let ast::Expression::MemberExpression(member_expr) = &call_expr.callee else {
-        return None;
-    };
-
-    let ast::MemberExpression::StaticMemberExpression(ref static_member_expr) = member_expr.0
-    else {
-        return None;
-    };
-
-    let ast::Expression::Identifier(object_ident) = &static_member_expr.object else {
-        return None;
-    };
-
-    let is_nls_localize_call =
-        object_ident.name == "nls" && static_member_expr.property.name == "localize";
-    if !is_nls_localize_call {
-        return None;
-    }
-
-    let ast::Argument::Expression(ast::Expression::StringLiteral(description_lit)) =
-        call_expr.arguments.get(1)?
-    else {
-        return None;
-    };
-
-    let color_name = color_name_lit.value.to_string();
-    let description = description_lit.value.to_string();
-
-    Some(ThemeColor {
-        name: color_name,
-        description,
-    })
 }
 
 fn color_field(group: Option<String>, color: ThemeColor) -> TokenStream {
